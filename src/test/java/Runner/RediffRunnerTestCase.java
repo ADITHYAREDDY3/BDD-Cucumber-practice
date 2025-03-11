2d5262292e2539ba714afcefa175406c38a28aeb
aditya <adityareddygooli@gmail.com>
2025-03-11 12:59:03 +0530

    chnaged in  runner cls

--- src/test/java/Runner/RediffRunnerTestCase.java
+++ src/test/java/Runner/RediffRunnerTestCase.java
@@ -2,14 +2,17 @@ package Runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-
 @CucumberOptions(
-	    features = {
-	                "src/test/resources/featureFiles/rediffCreateAcct.feature"
-				}
-, glue = { "stepDefinitions",
-				"hooks" }, plugin = { "pretty", "html:Reports/cucumber-reports.html", "json:target/cucumber.json" })
-
+    features = {
+                "src/test/resources/featureFiles/rediffCreateAcct.feature"
+    }, 
+    glue = { "stepDefinitions", "hooks" }, 
+    plugin = { 
+        "pretty", 
+        "html:Reports/cucumber-reports.html",
+        "json:Reports/cucumber-reports.json"
+    }
+)
 public class RediffRunnerTestCase extends AbstractTestNGCucumberTests {
 
 }
